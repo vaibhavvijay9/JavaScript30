2da116f0b6fe3028cd11b570d29d957c6b20b10d
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2018-01-12 00:59:12 +0530

    06 - run on console: findMatches("york",cities) and see result

--- 06-Type Ahead/index.js
+++ 06-Type Ahead/index.js
@@ -1,8 +1,8 @@
 // a github user's json file----> cities.json
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-var prom = fetch(endpoint); // fetch() is a predefined javascript method,returns "promise" i.e. something will be returned by api, & endpoint is also pre-defined
-console.log(prom); // promise
+// var prom = fetch(endpoint); // fetch() is a predefined javascript method,returns "promise" i.e. something will be returned by api, & endpoint is also pre-defined
+// console.log(prom); // promise
 
 const cities = [];
 fetch(endpoint).then(blob => blob.json())
@@ -13,5 +13,6 @@ function findMatches(wordToMatch, cities) {
         // here we have to check whether a city or state matches what was searched 
         const regex = new RegExp(wordToMatch, "gi") // gi --> global,insensitive(uppercase/lowercase)
         return place.city.match(regex) || place.state.match(regex)
+            // on colsole type: findMatches("york",cities) and see output
     });
 }
