66507df3b8f2f5132d27fd3be80631d42e680e4d
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2018-01-12 00:45:27 +0530

    06 - blob, parsing raw into json(), then, spread array using...

--- 06-Type Ahead/index.js
+++ 06-Type Ahead/index.js
@@ -1,1 +1,10 @@
+// a github user's json file----> cities.json
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+var prom = fetch(endpoint); // fetch() is a predefined javascript method,returns "promise" i.e. something will be returned by api, & endpoint is also pre-defined
+console.log(prom); // promise
+
+const cities = [];
+fetch(endpoint).then(blob => blob.json())
+    .then(data => cities.push(...data)) // ... --> to spread an array
+console.log(cities);
