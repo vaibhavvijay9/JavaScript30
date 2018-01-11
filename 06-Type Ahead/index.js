9c38fd60a3fd4195acf5091a8a16e362a772415a
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2018-01-12 01:15:55 +0530

    06 - passed values & got desired result
    keyup--->displayMatches() and so on...

--- 06-Type Ahead/index.js
+++ 06-Type Ahead/index.js
@@ -5,14 +5,26 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 // console.log(prom); // promise
 
 const cities = [];
-fetch(endpoint).then(blob => blob.json())
+fetch(endpoint).then(blob => blob.json()) // from blob i.e. raw data to json: blob.json()
     .then(data => cities.push(...data)) // ... --> to spread an array
 
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         // here we have to check whether a city or state matches what was searched 
-        const regex = new RegExp(wordToMatch, "gi") // gi --> global,insensitive(uppercase/lowercase)
+        const regex = new RegExp(wordToMatch, "gi") // gi --> global,case insensitive
         return place.city.match(regex) || place.state.match(regex)
             // on colsole type: findMatches("york",cities) and see output
     });
 }
+
+function displayMatches() {
+    //console.log(this.value); ---->  gives us value of the search input field
+    const matchArray = findMatches(this.value, cities);
+    console.log(matchArray);
+
+}
+
+const searchInput = document.querySelector(".search");
+const suggestion_list = document.querySelector(".suggestions");
+
+searchInput.addEventListener("keyup", displayMatches);
