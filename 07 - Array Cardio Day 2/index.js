87ffe66107eb0d3ab2e4f63b86773f452053ad89
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2018-01-12 19:57:59 +0530

    07 - with & without =>(arrow )

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -8,11 +8,11 @@ const people = [
 ];
 
 const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
+    { text: 'Love this!', id: 101 },
+    { text: 'Super good', id: 102 },
+    { text: 'You are the best', id: 103 },
+    { text: 'Ramen is my fav food ever', id: 104 },
+    { text: 'Nice Nice Nice!', id: 105 }
 ];
 
 // Some and Every Checks
@@ -26,15 +26,24 @@ console.log("Is at least one person 19 or older?: ", isAdult);
 
 // Array.prototype.every() // is everyone 19 or older?
 const allAdult = people.every(person => {
-    (new Date()).getFullYear() - person.year >= 19; // used date directly
+    (new Date()).getFullYear() - person.year >= 19; // used date directly & implicit return
 });
 console.log("Is everyone 19 or older?: ", allAdult);
 
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
-// find the comment with the ID of 823423
+// find the comment with the ID of 102
+
+// const comment = comments.find(function(comment) {                // without =>(arrow)
+//     if (comment.id == 102)
+//         return true;
+// });
+
+const comm = comments.find(comment => comment.id == 102);
+console.log(comm);
+
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
-// delete the comment with the ID of 823423
+// delete the comment with the ID of 102
